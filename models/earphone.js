5a65cb82d54d3950fd40deefe99194e45aadd364
charithtagore <122634098+charithtagore@users.noreply.github.com>
2023-04-22 03:38:21 -0500

    Done with the changes

--- models/earphone.js
+++ models/earphone.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose")
 const earphonesSchema = mongoose.Schema({
 brand: String,
 color: String,
-price: Number
+price:{
+    type:Number,
+    min: 1,
+    max: 7000 }
 })
 module.exports = mongoose.model("Costume",earphonesSchema)
--- routes/earphone.js
+++ routes/earphone.js
@@ -2,6 +2,17 @@ var express = require('express');
 const earphone_controlers= require('../controllers/earphone');
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on 
+// or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  }
+
 /* GET home page. */
 router.get('/', earphone_controlers.earphone_view_all_Page );
 
@@ -9,12 +20,12 @@ router.get('/', earphone_controlers.earphone_view_all_Page );
 router.get('/detail', earphone_controlers.earphone_view_one_Page);
 
 /* GET create earphone page */
-router.get('/create', earphone_controlers.earphone_create_Page);
+router.get('/create',secured, earphone_controlers.earphone_create_Page);
 
 /* GET create update page */
-router.get('/update', earphone_controlers.earphone_update_Page);
+router.get('/update',secured, earphone_controlers.earphone_update_Page);
 
 /* GET delete earphone page */
-router.get('/delete', earphone_controlers.earphone_delete_Page);
+router.get('/delete',secured, earphone_controlers.earphone_delete_Page);
 
 module.exports = router;
